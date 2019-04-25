8a5de5eec6db0f20d813d56000c4b4b7f7a33ad6
Duncan Hobbs <duncan2012@gmail.com>
2019-04-25 18:19:18 -0400

    Add test for resource constraint equation. Fix some errors in other tests.

--- open_cge/tests/test_aggregates.py
+++ open_cge/tests/test_aggregates.py
@@ -1,16 +1,18 @@
 ## This file tests the functiuons in aggregates.py to check they return expected outputs
 
 import pytest
+import math
 import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
+from numpy.testing import assert_allclose
 from open_cge import aggregates
 
 # Total investment
 def test_eqXg():
     g = 0.03
-    kk = 100
-    expected_XXg = 3
+    kk = 100.0
+    expected_XXg = 3.0
     test_XXg = aggregates.eqXg(g, kk)
     print('Type = ', type(test_XXg))
     assert expected_XXg == test_XXg
@@ -21,29 +23,29 @@ def test_eqSp():
     ssp = 0.1
     pf = np.array([5, 6, 3, 4])
     Ff = np.array([6, 4, 10, 5])
-    Fsh = 5
-    Trf = 1
-    expected_Sp = 10
+    Fsh = 5.0
+    Trf = 1.0
+    expected_Sp = 10.0
     test_Sp = aggregates.eqSp(ssp, pf, Ff, Fsh, Trf)
     assert expected_Sp == test_Sp
 
 
 # Domestic capital holdings
 def test_Kd():
-    g = 0.05
-    Sp = 100
+    g = 0.03
+    Sp = 15.0
     lam = np.array([0.3, 0.2, 0.4, 0.1])
-    pq = np.array([2, 4, 6, 8])
+    pq = np.array([3, 5, 6, 7])
     assert sum(lam) == 1
-    expected_Kd = 100
+    expected_Kd = 100.0
     test_Kd = aggregates.eqKd(g, Sp, lam, pq)
-    assert expected_Kd == test_Kd
+    assert math.isclose(expected_Kd, test_Kd)
 
 # Foreign holdings of domestically used capital
 def test_eqKf():
-    Kk = 110
-    Kd = 100
-    expected_Kf = 10
+    Kk = 110.0
+    Kd = 100.0
+    expected_Kf = 10.0
     test_Kf = aggregates.eqKf(Kk, Kd)
     assert expected_Kf == test_Kf
 
@@ -54,14 +56,14 @@ def test_eqKk():
 
 # Balance of payments
 def test_eqbop():
-    pWe = np.array( [2, 4, 6, 7])
-    pWm = np.array([3, 6, 2, 5])
-    E = np.array([6, 8, 10, 12])
-    M = np.array([10, 6, 12, 14])
-    Sf = 10
-    Fsh = 5
+    pWe = np.array( [2, 3, 6, 7])
+    pWm = np.array([4, 6, 2, 5])
+    E = np.array([5, 8, 10, 12])
+    M = np.array([10, 6, 12, 15])
+    Sf = 4.0
+    Fsh = 10.0
     er = 2.0
-    expected_bop_error = 29
+    expected_bop_error = 0.0
     test_bop_error = aggregates.eqbop(pWe, pWm, E, M, Sf, Fsh, er)
     assert expected_bop_error == test_bop_error
 
@@ -72,24 +74,31 @@ def test_eqSf():
     lam = np.array([0.2, 0.3, 0.2, 0.3])
     pq = np.array([4, 6, 3, 5])
     assert sum(lam) == 1
-    expected_Sf = 54
-    test_Sf = aggregates.eqSf(g, Kf, lam, pq)
-
+    expected_Sf = 14.1
+    test_Sf = aggregates.eqSf(g, lam, pq, Kf)
+    print(test_Sf)
+    assert_allclose(expected_Sf, test_Sf)
 
 # Resource constraint
 def test_eqpqerror():
+    Q = np.array([30, 40, 50, 60])
+    Xp = np.array([10, 15, 30, 35])
+    Xg = np.array([4, 6, 7, 9])
+    Xv = np.array([6, 4, 3, 6])
+    X = np.array([[2, 4, 3, 1], [4, 4, 2, 5], [4, 2, 3, 1], [2, 1, 3, 4]])
+    expected_pq_error = np.array([0, 0, 0, 0])
+    test_pq_error = aggregates.eqpqerror(Q, Xp, Xg, Xv, X)
+    assert_allclose(expected_pq_error, test_pq_error)
+
 
 
 # Comparing labor supply from the model to that in the data
-def test_eqpf():
+#def test_eqpf():
 
 
 # Comparing capital demand in the model and data
-def test_eqpk():
+#def test_eqpk():
 
 
 # Total investment
-def eqXXv():
-
-
-#
+#def eqXXv():
