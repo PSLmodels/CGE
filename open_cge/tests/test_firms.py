ff4a5a924f2a142ccb704f29310ba64a2c0b5722
Duncan Hobbs <duncan2012@gmail.com>
2019-04-26 16:10:52 -0400

    Add tests for eq.Y and eq.Xv to test_firms.py

--- open_cge/tests/test_firms.py
+++ open_cge/tests/test_firms.py
@@ -5,27 +5,51 @@ import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from open_cge import firms
+from numpy.testing import assert_allclose
 
 # Test production function output
 def test_eqpy():
-    b = np.array([0.3, 0.3, 0.4])
-    F = np.array([], [], [])
-    beta = np.array([], [], [])
+    b = np.array([0.6, 0.4])
+    assert sum(b) == 1
+    F = np.array([2, 5], [6, 3])
+    beta = np.array([0.3, 0.7], [0.6, 0.4])
     Y = np.array([])
+    expected_py_error =
+    test_py_error = firms.eqpy(b, F, beta, Y)
 
 # Test demand for intermediate inputs
 def test_eqX(ax, Z):
-    ax = np.array([], [], [])
-
+    ax = np.array([0.3, 0.7], [0.7, 0.3])
+    Z = np.array(:[20, 40])
+    expected_X = np.array([6, 2])
+    test_X = firms.eqX(ax, Z)
+    assert_allclose(expected_X, test_X)
 
 # Test value added
-
+def test_eqY(ay, Z):
+    ay = np.array([])
+    Z = np.array([])
+    expected_Y = np.array([])
+    test_Y = firms.eqY(ay, Z)
+    assert_allclose(expected_Y, test_Y)
 
 # Test output prices
-
+def test_eqpz():
+    ay =  np.array([0.2, 0.8], [0.8, 0.2])
+    ax = np.array([0.4, 0.6], [0.6, 0.4])
+    py = np.array([5, 7])
+    pq = np.array([12, 16])
+    expected_pz = np.array([])
+    test_pz = firms.eqpz(ay, ax, py, pq)
+    assert_allclose(expected_pz, test_pz)
 
 # Test investment demand for each good
-
+def test_eqXv():
+    lam = np.array([0.4, 0.6])
+    XXv = 100
+    expected_Xv = np.array([40, 60])
+    test_Xv = firms.eqXv(lam, XXv)  
+    assert_allclose(expected_Xv, test_Xv)
 
 # Test repatriated profits
 
