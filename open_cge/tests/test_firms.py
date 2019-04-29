9c84ef10f819cade693d166b603daaff646c3390
Duncan Hobbs <duncan2012@gmail.com>
2019-04-28 21:31:28 -0400

    Add tests for eqpe and eqpm in from firms.py.

--- open_cge/tests/test_firms.py
+++ open_cge/tests/test_firms.py
@@ -19,15 +19,16 @@ def test_eqpy():
     assert_allclose(expected_py_error, test_py_error, rtol = 1e-5)
 
 # Test demand for intermediate inputs
-def test_eqX(ax, Z):
+def test_eqX():
     ax = np.array([[0.3, 0.7], [0.6, 0.4]])
     Z = np.array([20, 40])
-    expected_X = np.array([6, 2])
+    Z.reshape(2, 1)
+    expected_X = np.array([[6, 28], [12, 16]])
     test_X = firms.eqX(ax, Z)
     assert_allclose(expected_X, test_X)
 
 # Test value added
-def test_eqY(ay, Z):
+def test_eqY():
     ay = np.array([0.1, 0.3])
     Z = np.array([20, 30])
     expected_Y = np.array([2, 9])
@@ -53,13 +54,29 @@ def test_eqXv():
     assert_allclose(expected_Xv, test_Xv)
 
 # Test repatriated profits
-
+def test_eqFsh():
+    R = 0.02
+    Kf = 100.0
+    er = 2.0
+    espected_Fsh = 4.0
+    test_Fsh = firms.eqFsh(R, Kf, er)
+    assert_allclose(espected_Fsh, test_Fsh)
 
 # Test export prices
-
+def test_eqpe():
+    er = 2.0
+    pWe = np.array([2, 6, 12, 15])
+    expected_pe = np.array([4, 12, 24, 30])
+    test_pe = firms.eqpe(er, pWe)
+    assert_allclose(expected_pe, test_pe)
 
 # Test import prices
-
+def test_eqpm():
+    er = 2.0
+    pWm = np.array([3, 7, 13, 22])
+    expected_pm = np.array([6, 14, 26, 44])
+    test_pm = firms.eqpm(er, pWm)
+    assert_allclose(expected_pm, test_pm)
 
 # Test CES production for importing firms
 
