48a94222249b4caa0b720187e9385649af4ae1e4
jdebacker <jason.debacker@gmail.com>
2019-05-07 16:25:30 -0400

    remove unused stuff from simpleCGE

--- open_cge/simpleCGE.py
+++ open_cge/simpleCGE.py
@@ -1,26 +1,10 @@
 # import packages
-import scipy.optimize as opt
 import numpy as np
-import pandas as pd
 from pandas import Series, DataFrame
-import os
 from open_cge import government as gov
 from open_cge import household as hh
 from open_cge import aggregates as agg
 from open_cge import firms
-from open_cge import calibrate
-
-# load social accounting matrix
-current_path = os.path.abspath(os.path.dirname(__file__))
-sam_path = os.path.join(current_path, 'SAM.xlsx')
-sam = pd.read_excel(sam_path, index_col=0, header=0)
-
-# declare sets
-u = ('AGR', 'OIL', 'IND', 'SER', 'LAB', 'CAP', 'LAND', 'NTR',
-     'DTX', 'IDT', 'ACT', 'HOH', 'GOV', 'INV', 'EXT')
-ind = ('AGR', 'OIL', 'IND', 'SER')
-h = ('LAB', 'CAP', 'LAND', 'NTR')
-w = ('LAB', 'LAND', 'NTR')
 
 
 def cge_system(pvec, args):
@@ -42,30 +26,17 @@ def cge_system(pvec, args):
     py = Series(py, index=list(ind))
     pf = Series(pf, index=list(h))
 
-    pe = firms.eqpe(er, d.pWe)
     pm = firms.eqpm(er, d.pWm)
     pq = firms.eqpq(pm, pd, p.taum, p.eta, p.deltam, p.deltad, p.gamma)
-    pz = firms.eqpz(p.ay, p.ax, py, pq)
     Kk = agg.eqKk(pf, Ff, R, p.lam, pq)
-    XXv = agg.eqXXv(d.g, Kk)
-    Xv = firms.eqXv(p.lam, XXv)
-    Xg = gov.eqXg(p.mu, XXg)
     Kf = agg.eqKf(Kk, Kd)
     Fsh = firms.eqFsh(R, Kf, er)
-    Td =gov.eqTd(p.taud, pf, Ff)
+    Td = gov.eqTd(p.taud, pf, Ff)
     Trf = gov.eqTrf(p.tautr, pf, Ff)
-    Tz = gov.eqTz(p.tauz, pz, Z)
-    X = firms.eqX(p.ax, Z)
     Y = firms.eqY(p.ay, Z)
     F = hh.eqF(p.beta, py, Y, pf)
     Sp = agg.eqSp(p.ssp, pf, Ff, Fsh, Trf)
     I = hh.eqI(pf, Ff, Sp, Td, Fsh, Trf)
-    Xp = hh.eqXp(p.alpha, I, pq)
-    E = firms.eqE(p.theta, p.xie, p.tauz, p.phi, pz, pe, Z)
-    D = firms.eqDex(p.theta, p.xid, p.tauz, p.phi, pz, pd, Z)
-    M = firms.eqM(p.gamma, p.deltam, p.eta, Q, pq, pm, p.taum)
-    Tm = gov.eqTm(p.taum, pm, M)
-
 
     pf_error = agg.eqpf(F, d.Ff0)
     pk_error = agg.eqpk(F, Kk, d.Kk0, d.Ff0)
@@ -81,100 +52,5 @@ def cge_system(pvec, args):
     pf_error = pf_error.values
 
     p_error = np.append(py_error, pf_error)
+
     return p_error
-
-
-# solve cge_system
-dist = 10
-tpi_iter = 0
-tpi_max_iter = 1000
-tpi_tol = 1e-10
-xi = 0.1
-
-# pvec = pvec_init
-pvec = np.ones(len(ind) + len(h))
-
-# Load data and parameters classes
-d = calibrate.model_data(sam, h, u, ind)
-p = calibrate.parameters(d, ind)
-
-XXg = d.XXg0
-R = d.R0
-er = 1
-
-Zbar = d.Z0
-Ffbar = d.Ff0
-Kdbar = d.Kd0
-Qbar = d.Q0
-pdbar = pvec[0:len(ind)]
-
-pm = firms.eqpm(er, d.pWm)
-test = firms.eqpq(pm, pdbar, p.taum, p.eta, p.deltam, p.deltad, p.gamma)
-
-
-'''
-#checking calibration of model
-cge_args = [p, d, ind, h, Zbar, Qbar, Kdbar, pdbar, Ffbar, R, er]
-errors = cge_system(pvec, cge_args)
-#---------------------------------------------
-
-
-'''
-
-while (dist > tpi_tol) & (tpi_iter < tpi_max_iter):
-	tpi_iter += 1
-	cge_args = [p, d, ind, h, Zbar, Qbar, Kdbar, pdbar, Ffbar, R, er]
-	results = opt.root(cge_system, pvec, args=cge_args, method='lm',
-					   tol=1e-5)
-	pprime = results.x
-	pyprime = pprime[0:len(ind)]
-	pfprime = pprime[len(ind):len(ind) + len(h)]
-	pyprime = Series(pyprime, index=list(ind))
-	pfprime = Series(pfprime, index=list(h))
-
-	pvec = pprime
-
-	temp = cge_system(pvec, cge_args)
-
-	pe = firms.eqpe(er, d.pWe)
-	pm = firms.eqpm(er, d.pWm)
-	pq = firms.eqpq(pm, pdbar, p.taum, p.eta, p.deltam, p.deltad, p.gamma)
-	pz = firms.eqpz(p.ay, p.ax, pyprime, pq)
-	Kk = agg.eqKk(pfprime, Ffbar, R, p.lam, pq)
-	Td = gov.eqTd(p.taud, pfprime, Ffbar)
-	Trf = gov.eqTrf(p.tautr, pfprime, Ffbar)
-	Tz = gov.eqTz(p.tauz, pz, Zbar)
-	Kf = agg.eqKf(Kk, Kdbar)
-	Fsh = firms.eqFsh(R, Kf, er)
-	Sf = agg.eqSf(d.g, p.lam, pq, Kf)
-	Sp = agg.eqSp(p.ssp, pfprime, Ffbar, Fsh, Trf)
-	I = hh.eqI(pfprime, Ffbar, Sp, Td, Fsh, Trf)
-	Xp = hh.eqXp(p.alpha, I, pq)
-	E = firms.eqE(p.theta, p.xie, p.tauz, p.phi, pz, pe, Zbar)
-	D = firms.eqDex(p.theta, p.xid, p.tauz, p.phi, pz, pdbar, Zbar)
-	M = firms.eqM(p.gamma, p.deltam, p.eta, Qbar, pq, pm, p.taum)
-	Qprime = firms.eqQ(p.gamma, p.deltam, p.deltad, p.eta, M, D)
-	pdprime = firms.eqpd(p.gamma, p.deltam, p.eta, Qprime, pq, D)
-	Zprime = firms.eqZ(p.theta, p.xie, p.xid, p.phi, E, D)
-	#    Zprime = Zprime.iloc[0]
-	Kdprime = agg.eqKd(d.g, Sp, p.lam, pq)
-	Ffprime = d.Ff0
-	# Ffprime['CAP'] = R * d.Kk * (p.lam * pq).sum() / pf[1]
-	Ffprime['CAP'] = R * Kk * (p.lam * pq).sum() / pfprime[1]
-
-	dist = (((Zbar - Zprime) ** 2 ) ** (1 / 2)).sum()
-	print('Distance at iteration ', tpi_iter, ' is ', dist)
-	pdbar = xi * pdprime + (1 - xi) * pdbar
-	Zbar = xi * Zprime + (1 - xi) * Zbar
-	Kdbar = xi * Kdprime + (1 - xi) * Kdbar
-	Qbar = xi * Qprime + (1 - xi) * Qbar
-	Ffbar = xi * Ffprime + (1 - xi) * Ffbar
-
-	bop_error = agg.eqbop(d.pWe, d.pWm, E, M, Sf, Fsh, er)
-
-	pd = firms.eqpd(p.gamma, p.deltam, p.eta, Qprime, pq, D)
-	Z = firms.eqZ(p.theta, p.xie, p.xid, p.phi, E, D)
-	Kd = agg.eqKd(d.g, Sp, p.lam, pq)
-	Q = firms.eqQ(p.gamma, p.deltam, p.deltad, p.eta, M, D)
-
-print('Model solved, Q = ', Q)
